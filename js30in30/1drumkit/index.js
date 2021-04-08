0965d735a1b3a685037425fdbe9421c352fa482f
Michael Fuery <mfuery@gmail.com>
2021-04-08 14:53:41 -0700

    Drumkit animation.

--- js30in30/1drumkit/index.js
+++ js30in30/1drumkit/index.js
@@ -5,21 +5,35 @@ window.addEventListener("DOMContentLoaded", (e) => {
       return null
     }
 
-    audioTag.pause()
+    const domTarget = document.querySelector(`.key[data-key=${key}]`)
+    domTarget.classList.add("playing")
+
     audioTag.currentTime = 0.0
     audioTag.play()
   }
 
-  document.querySelectorAll(".key").forEach((el, i) => {
+  // Initialize DOM events
+  document.querySelectorAll(".key").forEach((el) => {
     el.addEventListener("click", (e) => {
       const keyHit = e.target.dataset.key
       playClip(keyHit)
     })
+    el.addEventListener("transitionend", (e) => {
+      if (e.propertyName !== "transform") {
+        return
+      }
+
+      e.target.classList.remove("playing")
+    })
   })
+
+  // Keyboard events
   document.addEventListener("keydown", (e) => {
     const keyHit = e.key.toUpperCase()
     playClip(keyHit)
   })
+
+  // Touch events
   document.addEventListener("touchstart", (e) => {
     const keyHit = e.key.toUpperCase()
     playClip(keyHit)
